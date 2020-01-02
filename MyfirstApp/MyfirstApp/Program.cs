d14080d209d53e869e3a282c63b53ec1f3a9bc03
ZhongZhaoJun <707500781@qq.com>
2020-01-02 17:21:40 +0800

    no message

---
+++ MyfirstApp/MyfirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyfirstApp //命名空间
+{
+    class Program //类名
+    {
+        static void Main(string[] args) //Main方法，程序入口
+        {
+            Console.WriteLine("Hello World!"); //命令，以分号结束
+            //快捷键：ctrl+F5 直接运行
+        }
+    }
+}
